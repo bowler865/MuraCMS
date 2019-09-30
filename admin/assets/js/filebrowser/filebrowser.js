8a40a6a08fc09f9df25ee821bf653302a7d1c826
Grant <grant.shepert@blueriver.com>
2019-09-30 17:58:16 +0100

    added fixed ratio

--- admin/assets/js/filebrowser/filebrowser.js
+++ admin/assets/js/filebrowser/filebrowser.js
@@ -1106,7 +1106,6 @@ config: {
     props: ["currentFile","currentIndex","imageEditTarget","settings"],
     template: `
        <div>
-       |{{this.fixedwidth}}|
        <div v-if="this.editmode  == 'RESIZE'" class="fileviewer-image" id="imagediv" :style="{ 'background-image': 'url(' + encodeURI(currentFile.url) + '?' + Math.ceil(Math.random()*100000) + ')' }"></div>
        <div v-if="this.editmode == ''" class="fileviewer-image-edit"><img style="min-height: 100%" id="fileviewer-image-editable" :src="currentFile.url"></div>
         <div class="fileviewer-edit-menu mura-actions">
